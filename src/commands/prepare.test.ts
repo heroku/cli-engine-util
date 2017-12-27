ffaa434ced9c2c7249a24b0934cd34a3efce6e13
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-12-26 20:26:13 -0800

    fix test

--- src/commands/prepare.test.ts
+++ src/commands/prepare.test.ts
@@ -20,5 +20,5 @@ test('ok', async () => {
   await Prepare.mock()
   expect(fs.del).toBeCalledWith('lib')
   expect(util.sh).toBeCalledWith('tsc')
-  expect(fs.del).toBeCalledWith('lib/**/*.test.+(d.ts|js)')
+  expect(fs.del).toBeCalledWith(['lib/**/*.test.+(d.ts|js)', 'lib/**/__test__'])
 })
