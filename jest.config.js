f072277121a696f27501d8a36780d81d65945f27
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-01-07 01:08:48 -0800

    ignore some files from coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   setupTestFrameworkScriptFile: "<rootDir>/src/__test__/init.ts",
   globalSetup: "<rootDir>/src/__test__/setup.js",
+  coveragePathIgnorePatterns: ['/node_modules/', '<rootDir>/__test__'],
   mapCoverage: true,
   moduleFileExtensions: ['ts', 'js'],
   testMatch: ['<rootDir>/src/**/*.test.ts'],
